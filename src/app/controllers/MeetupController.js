34f9ab4418b756f985f7886e65cd15ba61aaa764
Rafael <rafaelbpa@gmail.com>
2019-07-02 14:01:39 -0300

    Creating meetups with validations

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,42 @@
+import { isBefore, startOfHour, parseISO } from 'date-fns';
+import * as Yup from 'yup';
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      file_id: Yup.number().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { title, description, location, date, file_id } = req.body;
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(400)
+        .json({ error: "You can't subscribe to a past event" });
+    }
+
+    const meetup = await Meetup.create({
+      title,
+      description,
+      location,
+      date,
+      file_id,
+      user_id: req.userId,
+    });
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();
