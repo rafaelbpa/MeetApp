ba5fe4b01ceb66a3a1e33a1f8b1639d559a564c7
Rafael <rafaelbpa@gmail.com>
2019-07-02 15:33:59 -0300

    Deleting with validations

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -89,6 +89,26 @@ class MeetupController {
 
     return res.json(meetup);
   }
+
+  async delete(req, res) {
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (!meetup) {
+      return res.status(404).json({ error: 'Not found.' });
+    }
+
+    if (meetup.user_id !== req.userId) {
+      return res.status(401).json({ error: 'Not authorized.' });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({ error: "Can't delete past meetups." });
+    }
+
+    await meetup.destroy();
+
+    return res.send();
+  }
 }
 
 export default new MeetupController();
